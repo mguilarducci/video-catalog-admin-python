bc0f0204ef86c5a1c8fcdbb70eae4f8d5276fdbd
Matheus Guilarducci <mguilarducci@gmail.com>
2023-01-29 16:54:47 +0000

    feat(category): :sparkles: create category
    create category entity with some operations (update, activate, deactivate)

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,4 +1,25 @@
-class Category:
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
 
-    def __init__(self, name) -> None:
-        self.name = name
+from __shared.domain.entities import Entity
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class Category(Entity):
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    created_at: Optional[datetime] = field(
+        default_factory=datetime.now
+    )
+
+    def update(self, name: str, description: str) -> None:
+        self._set('name', name)
+        self._set('description', description)
+
+    def activate(self) -> None:
+        self._set('is_active', True)
+
+    def deactivate(self) -> None:
+        self._set('is_active', False)
--- src/category/tests/unit/domain/test_unit_entities.py
+++ src/category/tests/unit/domain/test_unit_entities.py
@@ -1,6 +1,66 @@
+from dataclasses import FrozenInstanceError, is_dataclass
+from datetime import datetime
 from unittest import TestCase
+from __shared.domain.entities import Entity
+
+from category.domain.entities import Category
 
 
-class EmptyTest(TestCase):
-    def test_assert_true(self):
-        self.assertTrue(True)
+class CategoryUnitTest(TestCase):
+    def test_should_be_a_dataclass(self):
+        self.assertTrue(is_dataclass(Category))
+
+    def test_should_be_a_subclass_of_entity(self):
+        self.assertTrue(issubclass(Category, Entity))
+
+    def test_constructor(self):
+        category = Category(name='Movie')
+
+        self.assertEqual('Movie', category.name)
+        self.assertIsNone(category.description)
+        self.assertTrue(category.is_active)
+        self.assertIsInstance(category.created_at, datetime)
+
+        created_at = datetime.now()
+        category2 = Category(
+            name='Movie1',
+            description='some description',
+            is_active=False,
+            created_at=created_at
+        )
+
+        self.assertEqual('Movie1', category2.name)
+        self.assertEqual('some description', category2.description)
+        self.assertFalse(category2.is_active)
+        self.assertEqual(created_at, category2.created_at)
+
+    def test_if_created_at_is_generated_in_constructor(self):
+        category1 = Category(name='Movie 1')
+        category2 = Category(name='Movie 2')
+
+        self.assertNotEqual(
+            category1.created_at.timestamp(),
+            category2.created_at.timestamp()
+        )
+
+    def test_is_immutable(self):
+        with self.assertRaises(FrozenInstanceError):
+            value_object = Category(name='test')
+            value_object.name = 'fake id'
+
+    def test_should_update_name_and_description(self):
+        category = Category(name='name')
+        category.update('new name', 'new description')
+
+        self.assertEqual('new name', category.name)
+        self.assertEqual('new description', category.description)
+
+    def test_should_activate(self):
+        category = Category(name='name', is_active=False)
+        category.activate()
+        self.assertTrue(category.is_active)
+
+    def test_should_deactivate(self):
+        category = Category(name='name', is_active=True)
+        category.deactivate()
+        self.assertFalse(category.is_active)
