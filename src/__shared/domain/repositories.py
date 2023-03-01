6d1e8f66c15142e8e46844c898cd535189d5addc
Matheus Guilarducci <mguilarducci@gmail.com>
2023-03-01 12:08:24 +0000

    feat(shared): create search params

--- src/__shared/domain/repositories.py
+++ src/__shared/domain/repositories.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Generic, List, TypeVar
+from dataclasses import dataclass, field
+from typing import Any, Generic, List, Optional, TypeVar
 
 from __shared.domain.entities import Entity
 from __shared.domain.value_objects import UniqueEntityId
@@ -39,3 +40,76 @@ class SearchableRepositoryInterface(Generic[GenericEntity,
     @abstractmethod
     def search(self, params: GenericSearchableInput) -> GenericSearchableOutput:
         raise NotImplementedError()
+
+
+SearchFilter = TypeVar('SearchFilter', str, Any)
+
+
+@dataclass(slots=True, kw_only=True)
+class SearchParams(Generic[SearchFilter]):
+    DEFAULT_PAGE = 1
+    DEFAULT_ITEMS_PER_PAGE = 10
+    DEFAULT_ORDER_BY_FIELD = None
+    DEFAULT_ORDER_BY_DIRECTION = None
+    DEFAULT_FILTER = None
+
+    page: Optional[int] = field(
+        default_factory=lambda: SearchParams.DEFAULT_PAGE)
+    items_per_page: Optional[int] = field(
+        default_factory=lambda: SearchParams.DEFAULT_ITEMS_PER_PAGE)
+    order_by_field: Optional[str] = field(
+        default_factory=lambda: SearchParams.DEFAULT_ORDER_BY_FIELD)
+    order_by_direction: Optional[str] = field(
+        default_factory=lambda: SearchParams.DEFAULT_ORDER_BY_DIRECTION)
+    filter: Optional[SearchFilter] = field(
+        default_factory=lambda: SearchParams.DEFAULT_FILTER)
+
+    def __post_init__(self):
+        self._normalize_page()
+        self._normalize_items_per_page()
+        self._normalize_order_by_field()
+        self._normalize_order_by_direction()
+        self._normalize_filter()
+
+    def _normalize_page(self):
+        page = SearchParams._convert_to_int(self.page)
+        if page <= 0:
+            page = SearchParams.DEFAULT_PAGE
+
+        self.page = page
+
+    def _normalize_items_per_page(self):
+        items_per_page = SearchParams._convert_to_int(self.items_per_page)
+        if items_per_page < 1:
+            items_per_page = SearchParams.DEFAULT_ITEMS_PER_PAGE
+
+        self.items_per_page = items_per_page
+
+    def _normalize_order_by_field(self):
+        if self.order_by_field is not None:
+            self.order_by_field = None if self.order_by_field == '' \
+                else str(self.order_by_field)
+
+    def _normalize_order_by_direction(self):
+        if not self.order_by_field:
+            self.order_by_direction = None
+            return
+
+        direction = str(self.order_by_direction).lower()
+        if direction not in ['asc', 'desc']:
+            self.order_by_direction = 'asc'
+            return
+
+        self.order_by_direction = direction
+
+    def _normalize_filter(self):
+        if self.filter is not None:
+            self.filter = None if self.filter == '' \
+                else str(self.filter)
+
+    @staticmethod
+    def _convert_to_int(value: Any, default=0) -> int:  # pylint: disable=no-self-use
+        try:
+            return int(value)
+        except (ValueError, TypeError):
+            return default
--- src/__shared/tests/unit/domain/test_unit_repositories.py
+++ src/__shared/tests/unit/domain/test_unit_repositories.py
@@ -1,5 +1,7 @@
+from typing import Optional
 from unittest import TestCase
-from __shared.domain.repositories import RepositoryInterface, SearchableRepositoryInterface
+from __shared.domain.repositories import RepositoryInterface, SearchFilter, \
+    SearchParams, SearchableRepositoryInterface
 
 
 class RepositoryInterfaceUnitTest(TestCase):
@@ -22,3 +24,190 @@ class SearchableRepositoryInterfaceUnitTest(TestCase):
         expected_message = "Can't instantiate abstract class SearchableRepositoryInterface " + \
             "with abstract methods delete, find_all, find_by_id, insert, search, update"
         self.assertEqual(expected_message, error.exception.args[0])
+
+
+class SearchParamsUnitTest(TestCase):
+    def test_should_have_correct_props(self):
+        expected = {
+            'page': Optional[int],
+            'items_per_page': Optional[int],
+            'order_by_field': Optional[str],
+            'order_by_direction': Optional[str],
+            'filter': Optional[SearchFilter]
+        }
+
+        result = SearchParams.__annotations__
+
+        self.assertDictEqual(expected, result)
+
+    def test_should_have_correct_default_values(self):
+        search_params = SearchParams()
+
+        self.assertEqual(1, search_params.page,
+                         'SearchParams.page default value should be `1`')
+        self.assertEqual(10, search_params.items_per_page,
+                         'SearchParams.items_per_page default value should be `10`')
+        self.assertIsNone(search_params.order_by_field,
+                          'SearchParams.order_by_field default value should be `None`')
+        self.assertIsNone(search_params.order_by_direction,
+                          'SearchParams.order_by_direction default value should be `None`')
+        self.assertIsNone(search_params.filter,
+                          'SearchParams.filter default value should be `None`')
+
+    def test_page_prop_behavior(self):
+        cases = [
+            {'page': None, 'expected': 1},
+            {'page': "", 'expected': 1},
+            {'page': "fake", 'expected': 1},
+            {'page': 0, 'expected': 1},
+            {'page': -1, 'expected': 1},
+            {'page': "0", 'expected': 1},
+            {'page': "-1", 'expected': 1},
+            {'page': 5.5, 'expected': 5},
+            {'page': True, 'expected': 1},
+            {'page': False, 'expected': 1},
+            {'page': {}, 'expected': 1},
+            {'page': 1, 'expected': 1},
+            {'page': 2, 'expected': 2},
+        ]
+
+        for case in cases:
+            page = case.get('page')
+            expected = case.get('expected')
+            expected_message = f'`SearchParams(page={page})` should set the ' + \
+                f'`SearchParams.page` value to `{expected}`.'
+
+            search_params = SearchParams(page=page)
+
+            self.assertEqual(expected, search_params.page, expected_message)
+
+    def test_items_per_page_behavior(self):
+        cases = [
+            {'items_per_page': None, 'expected': 10},
+            {'items_per_page': "", 'expected': 10},
+            {'items_per_page': "fake", 'expected': 10},
+            {'items_per_page': 0, 'expected': 10},
+            {'items_per_page': -1, 'expected': 10},
+            {'items_per_page': "0", 'expected': 10},
+            {'items_per_page': "-1", 'expected': 10},
+            {'items_per_page': 5.5, 'expected': 5},
+            {'items_per_page': True, 'expected': 1},
+            {'items_per_page': False, 'expected': 10},
+            {'items_per_page': {}, 'expected': 10},
+            {'items_per_page': 1, 'expected': 1},
+            {'items_per_page': 2, 'expected': 2},
+        ]
+
+        for case in cases:
+            items_per_page = case.get('items_per_page')
+            expected = case.get('expected')
+            expected_message = f'`SearchParams(items_per_page={items_per_page})` ' + \
+                f'should set the `SearchParams.items_per_page` value to `{expected}`.'
+
+            search_params = SearchParams(items_per_page=items_per_page)
+
+            self.assertEqual(
+                expected, search_params.items_per_page, expected_message)
+
+    def test_order_by_field_behavior(self):
+        cases = [
+            {'order_by_field': None, 'expected': None},
+            {'order_by_field': "", 'expected': None},
+            {'order_by_field': "fake", 'expected': 'fake'},
+            {'order_by_field': 0, 'expected': '0'},
+            {'order_by_field': -1, 'expected': '-1'},
+            {'order_by_field': "0", 'expected': '0'},
+            {'order_by_field': "-1", 'expected': '-1'},
+            {'order_by_field': 5.5, 'expected': '5.5'},
+            {'order_by_field': True, 'expected': 'True'},
+            {'order_by_field': False, 'expected': 'False'},
+            {'order_by_field': {}, 'expected': '{}'},
+        ]
+
+        for case in cases:
+            order_by_field = case.get('order_by_field')
+            expected = case.get('expected')
+            expected_message = f'`SearchParams(order_by_field={order_by_field})` ' + \
+                f'should set the `SearchParams.order_by_field` value to `{expected}`.'
+
+            search_params = SearchParams(order_by_field=order_by_field)
+
+            self.assertEqual(
+                expected, search_params.order_by_field, expected_message)
+
+    def test_order_by_direction_behavior(self):
+        search_params = SearchParams(order_by_field=None)
+        expected_message = '`SearchParams(order_by_field=None)` ' + \
+            'should set the `SearchParams.order_by_direction` value to `None`.'  # NOSONAR
+        self.assertIsNone(
+            search_params.order_by_direction, expected_message)
+
+        search_params = SearchParams(
+            order_by_field=None, order_by_direction='asc')
+        expected_message = '`SearchParams(order_by_field=None, order_by_direction="asc")` ' + \
+            'should set the `SearchParams.order_by_direction` value to `None`.'
+        self.assertIsNone(
+            search_params.order_by_direction, expected_message)
+
+        search_params = SearchParams(order_by_field='')
+        expected_message = '`SearchParams(order_by_field="")` ' + \
+            'should set the `SearchParams.order_by_direction` value to `None`.'
+        self.assertIsNone(
+            search_params.order_by_direction, expected_message)
+
+        search_params = SearchParams(
+            order_by_field='', order_by_direction='asc')
+        expected_message = '`SearchParams(order_by_field="", order_by_direction="asc")` ' + \
+            'should set the `SearchParams.order_by_direction` value to `None`.'
+        self.assertIsNone(
+            search_params.order_by_direction, expected_message)
+
+        cases = [
+            {'order_by_direction': None, 'expected': 'asc'},
+            {'order_by_direction': "", 'expected': 'asc'},
+            {'order_by_direction': "fake", 'expected': 'asc'},
+            {'order_by_direction': 0, 'expected': 'asc'},
+            {'order_by_direction': {}, 'expected': 'asc'},
+            {'order_by_direction': 'asc', 'expected': 'asc'},
+            {'order_by_direction': 'ASC', 'expected': 'asc'},
+            {'order_by_direction': 'desc', 'expected': 'desc'},
+            {'order_by_direction': 'DESC', 'expected': 'desc'},
+        ]
+
+        for case in cases:
+            order_by_direction = case.get('order_by_direction')
+            expected = case.get('expected')
+            expected_message = f'`SearchParams(order_by_direction={order_by_direction})` ' + \
+                f'should set the `SearchParams.order_by_direction` value to `{expected}`.'
+
+            search_params = SearchParams(
+                order_by_field='field', order_by_direction=order_by_direction)
+
+            self.assertEqual(
+                expected, search_params.order_by_direction, expected_message)
+
+    def test_filter_behavior(self):
+        cases = [
+            {'filter': None, 'expected': None},
+            {'filter': "", 'expected': None},
+            {'filter': "fake", 'expected': 'fake'},
+            {'filter': 0, 'expected': '0'},
+            {'filter': -1, 'expected': '-1'},
+            {'filter': "0", 'expected': '0'},
+            {'filter': "-1", 'expected': '-1'},
+            {'filter': 5.5, 'expected': '5.5'},
+            {'filter': True, 'expected': 'True'},
+            {'filter': False, 'expected': 'False'},
+            {'filter': {}, 'expected': '{}'},
+        ]
+
+        for case in cases:
+            filter_param = case.get('filter')
+            expected = case.get('expected')
+            expected_message = f'`SearchParams(filter={filter_param})` ' + \
+                f'should set the `SearchParams.filter` value to `{expected}`.'
+
+            search_params = SearchParams(filter=filter_param)
+
+            self.assertEqual(
+                expected, search_params.filter, expected_message)
