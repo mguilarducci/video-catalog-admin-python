d6a156d1cef315590b5c0b07db42546632d039bd
Matheus Guilarducci <mguilarducci@gmail.com>
2023-03-03 12:27:53 +0000

    feat(shared): implement in memory searchable filter

--- src/__shared/infra/repositories.py
+++ src/__shared/infra/repositories.py
@@ -1,34 +1,72 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from typing import Dict, List
+from typing import Dict, Generic, List
 
 from __shared.domain.exceptions import NotFoundException
-from __shared.domain.repositories import GenericEntity, RepositoryInterface
+from __shared.domain.repositories import GenericEntity, RepositoryInterface, \
+    SearchFilter, SearchParams, SearchResult, SearchableRepositoryInterface
 from __shared.domain.value_objects import UniqueEntityId
 
 
 @dataclass(slots=True)
 class InMemoryRepository(RepositoryInterface[GenericEntity], ABC):
-    items: Dict[str, GenericEntity] = field(default_factory=lambda: {})
+    data: Dict[str, GenericEntity] = field(default_factory=lambda: {})
 
     def insert(self, entity: GenericEntity) -> None:
-        self.items.update({entity.id: entity})
+        self.data.update({entity.id: entity})
 
     def find_by_id(self, entity_id: str | UniqueEntityId) -> GenericEntity:
         self._raise_if_not_found(str(entity_id))
-        return self.items.get(str(entity_id))
+        return self.data.get(str(entity_id))
 
     def find_all(self) -> List[GenericEntity]:
-        return list(self.items.values())
+        return list(self.data.values())
 
     def update(self, entity: GenericEntity) -> None:
         self._raise_if_not_found(entity.id)
-        self.items.update({entity.id: entity})
+        self.data.update({entity.id: entity})
 
     def delete(self, entity_id: str | UniqueEntityId) -> None:
         self._raise_if_not_found(str(entity_id))
-        self.items.pop(str(entity_id))
+        self.data.pop(str(entity_id))
 
     def _raise_if_not_found(self, entity_id: str) -> None:
-        if entity_id not in self.items:
-            raise NotFoundException(f'Entity not found. data=[id: `{entity_id}`]')
+        if entity_id not in self.data:
+            raise NotFoundException(
+                f'Entity not found. data=[id: `{entity_id}`]')
+
+
+class InMemorySearchableRepository(Generic[GenericEntity, SearchFilter],
+                                   InMemoryRepository[GenericEntity],
+                                   SearchableRepositoryInterface[GenericEntity,
+                                                                 SearchParams[SearchFilter],
+                                                                 SearchResult[GenericEntity]],
+                                   ABC):
+    def search(self, search_params: SearchParams[SearchFilter]) -> SearchResult[GenericEntity]:
+        filtered_data = self._filter(self.data, search_params.filter)
+        ordered_data = self._order_by(filtered_data,
+                                      search_params.order_by_field,
+                                      search_params.order_by_direction)
+        paginated_data = self._paginate(ordered_data,
+                                        search_params.page,
+                                        search_params.items_per_page)
+
+        return SearchResult(count=len(filtered_data),
+                            items_per_page=search_params.items_per_page,
+                            current_page=search_params.page,
+                            data=paginated_data)
+
+    @abstractmethod
+    def _filter(self,
+                data: List[GenericEntity],
+                filter_param: SearchFilter | None) -> List[GenericEntity]:
+        raise NotImplementedError()
+
+    def _order_by(self,
+                  data: List[GenericEntity],
+                  order_by_field: str | None,
+                  order_by_direction: str | None) -> List[GenericEntity]:
+        pass
+
+    def _paginate(self, data: List[GenericEntity], page: int, per_page: int) -> List[GenericEntity]:
+        pass
--- src/__shared/tests/unit/infra/test_unit_repositories.py
+++ src/__shared/tests/unit/infra/test_unit_repositories.py
@@ -1,40 +1,41 @@
 from dataclasses import dataclass
+from typing import List
 from unittest import TestCase
 
 from __shared.domain.entities import Entity
 from __shared.domain.exceptions import NotFoundException
-from __shared.infra.repositories import InMemoryRepository
+from __shared.infra.repositories import InMemoryRepository, InMemorySearchableRepository
 
 
 @dataclass(slots=True, frozen=True, kw_only=True)
-class StubEntity(Entity):
+class EntityStub(Entity):
     name: str
     age: int
 
 
-class StubInMemoryRepository(InMemoryRepository[StubEntity]):
+class StubInMemoryRepository(InMemoryRepository[EntityStub]):
     pass
 
 
 class InMemoryRepositoryUnitTest(TestCase):
     repo: StubInMemoryRepository
-    item: StubEntity
+    item: EntityStub
 
     def setUp(self) -> None:
         self.repo = StubInMemoryRepository()
-        self.item = StubEntity(name='Stub name', age=20)
+        self.item = EntityStub(name='Stub name', age=20)
 
     def test_should_init_with_empty_data(self):
-        self.assertDictEqual({}, self.repo.items)
+        self.assertDictEqual({}, self.repo.data)
 
     def test_insert_should_insert_an_item(self):
         self.repo.insert(self.item)
-        self.assertDictEqual({self.item.id: self.item}, self.repo.items)
+        self.assertDictEqual({self.item.id: self.item}, self.repo.data)
 
     def test_insert_should_not_duplicate_an_item(self):
         self.repo.insert(self.item)
         self.repo.insert(self.item)
-        self.assertDictEqual({self.item.id: self.item}, self.repo.items)
+        self.assertDictEqual({self.item.id: self.item}, self.repo.data)
 
     def test_find_by_id_should_return_the_item(self):
         self.repo.insert(self.item)
@@ -56,18 +57,18 @@ class InMemoryRepositoryUnitTest(TestCase):
         items = self.repo.find_all()
         self.assertEqual([self.item], items)
 
-        new_item = StubEntity(name='new', age=30)
+        new_item = EntityStub(name='new', age=30)
         self.repo.insert(new_item)
         items = self.repo.find_all()
         self.assertEqual([self.item, new_item], items)
 
     def test_update_should_update_an_item(self):
         self.repo.insert(self.item)
-        item_updated = StubEntity(
+        item_updated = EntityStub(
             unique_entity_id=self.item.id, name='new name', age=50)
 
         self.repo.update(item_updated)
-        self.assertEqual(item_updated, self.repo.items.get(self.item.id))
+        self.assertEqual(item_updated, self.repo.data.get(self.item.id))
 
     def test_update_should_raise_an_exception_when_the_item_does_not_exist(self):
         with self.assertRaises(NotFoundException) as error:
@@ -79,7 +80,7 @@ class InMemoryRepositoryUnitTest(TestCase):
     def test_delete_should_delete_the_item(self):
         self.repo.insert(self.item)
         self.repo.delete(self.item.id)
-        self.assertDictEqual({}, self.repo.items)
+        self.assertDictEqual({}, self.repo.data)
 
     def test_delete_should_raise_an_error_when_the_item_does_not_exist(self):
         with self.assertRaises(NotFoundException) as error:
@@ -87,3 +88,45 @@ class InMemoryRepositoryUnitTest(TestCase):
 
         message_expected = f'Entity not found. data=[id: `{self.item.id}`]'
         self.assertEqual(message_expected, error.exception.args[0])
+
+
+class InMemorySearchableRepositoryStub(InMemorySearchableRepository[EntityStub, str]):
+    def _filter(self, data: List[EntityStub], filter_param: str | None) -> List[EntityStub]:
+        if not filter_param:
+            return data
+
+        filtered = filter(lambda item: filter_param.lower() in item.name.lower()
+                          or filter_param == str(item.age), data)
+
+        return list(filtered)
+
+
+class InMemorySearchableRepositoryUnitTest(TestCase):
+    repository: InMemorySearchableRepositoryStub
+
+    def setUp(self) -> None:
+        self.repository = InMemorySearchableRepositoryStub()
+
+    def test__filter_behavior(self):
+        # pylint: disable=protected-access
+        data = [EntityStub(name='name value te', age=20),
+                EntityStub(name='Name value', age=30),
+                EntityStub(name='test', age=40),]
+        result = self.repository._filter(data, None)
+        self.assertListEqual(data, result)
+
+        result = self.repository._filter(data, 'name')
+        expected = [data[0], data[1]]
+        self.assertListEqual(expected, result)
+
+        result = self.repository._filter(data, 'TEST')
+        expected = [data[2]]
+        self.assertListEqual(expected, result)
+
+        result = self.repository._filter(data, 'TE')
+        expected = [data[0], data[2]]
+        self.assertListEqual(expected, result)
+
+        result = self.repository._filter(data, '20')
+        expected = [data[0]]
+        self.assertListEqual(expected, result)
